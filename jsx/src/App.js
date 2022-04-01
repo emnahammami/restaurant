9ae37564a2ab3793b082ff14c10ae2176b001bc9
emna <hammamiemna22@gmail.com>
2022-04-01 09:24:02 +0100

    YOUR FIRST MESSAGE HERE

---
+++ jsx/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import '../src/style.css'
+import File from "./file"
+function App() {
+  return (
+    <div className="App">
+  <File/>
+    </div>
+  );
+}
+
+export default App;
