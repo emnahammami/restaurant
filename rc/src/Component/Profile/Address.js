1a2be28125af98d243c31038b6477d987e32af99
emnahammami <43586779+emnahammami@users.noreply.github.com>
2023-12-08 22:08:05 +0100

    Delete rc directory

--- rc/src/Component/Profile/Address.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Address() {
-  return (
-    <div>I live in hammem chatt</div>
-  )
-}
--- rc/src/Component/Profile/FullName.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function FullName() {
-  return (
-    <div>Hi !!my name is Emma</div>
-  )
-}
